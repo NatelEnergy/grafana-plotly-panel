ba8c808806ba3ce8ec486b7f10fb006e8c9814e0
Ryan McKinley <ryantxu@gmail.com>
2017-12-19 00:44:02 +0100

    adding decleration

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,11 +61,11 @@ module.exports = function(grunt) {
         options: {
           rootDir: "src",
           verbose: true,
-          declaration: false,
 
           "target": "ES5",
           "module": "system",
           "sourceMap": true,
+          "declaration": true,
           "emitDecoratorMetadata": true,
           "experimentalDecorators": true,
           "noImplicitAny": false,
