a309bcf01d7fbb6927f6699ea7b3b411d93e9935
Ryan McKinley <ryantxu@gmail.com>
2018-08-28 10:28:18 -0700

    load the plotly library from node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ReplaceInFileWebpackPlugin = require('replace-in-file-webpack-plugin');
+const CreateFileWebpack = require('create-file-webpack');
 
 const packageJson = require('./package.json');
 
@@ -28,6 +29,7 @@ module.exports = {
     'prismjs',
     'slate-plain-serializer',
     'slate-react',
+    './lib/plotly.min',
     function(context, request, callback) {
       var prefix = 'app/';
       if (request.indexOf(prefix) === 0) {
@@ -37,9 +39,16 @@ module.exports = {
     },
   ],
   plugins: [
+    // HACKS to put plotly.min.js in the distribution.. must be a better way
+    new CreateFileWebpack({
+      path: 'src/lib',
+      fileName: 'plotly.min.js',
+      content: '// just a stub so import is found, webpack will use the one is dist',
+    }),
     new CleanWebpackPlugin('dist', {allowExternal: true}),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new CopyWebpackPlugin([
+      {from: '../node_modules/plotly.js/dist/plotly.min.js', to: 'lib'},
       {from: 'plugin.json', to: '.'},
       {from: '../README.md', to: '.'},
       {from: '../LICENSE', to: '.'},
